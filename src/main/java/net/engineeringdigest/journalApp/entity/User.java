ff63e58ffe5a2c750ce11b553e111cdc1b943bdb
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-11-10 00:54:52 +0530

    user created

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -0,0 +1,27 @@
+package net.engineeringdigest.journalApp.entity;
+
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "journal_entries")
+@Data
+public class User {
+    @Id
+    private ObjectId id;
+    @NonNull
+    @Indexed(unique = true)
+    private String userName;
+    @NonNull
+    private String password;
+    private List<JournalEntry> journalEntries = new ArrayList<>();
+
+}
